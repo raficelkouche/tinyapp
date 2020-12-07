e8988a887b4ae4ac91de4d492bf5228dce3ed551
Rafic El Kouche <raficelkouche@hotmail.com>
2020-12-07 18:03:53 -0500

    added a new endpoint /urls.json

--- express_server.js
+++ express_server.js
@@ -11,6 +11,10 @@ app.get("/", (req,res) => {
   res.send("hello");
 })
 
+app.get("/urls.json", (req,res) => {
+  res.json(urlDatabase);
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 })
